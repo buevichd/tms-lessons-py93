d6b99f225bcd19e410990c1c0c68c1697cc81f0a
Dmitry Buevich <buevichd@gmail.com>
2023-09-26 17:07:03 +0300

    Rename files

--- lesson_04/task_03.py
+++ lesson_04/task_03.py
@@ -1,8 +1,10 @@
-is_moving = input('Is it moving? ')
-should_move = input('Should it move? ')
-if is_moving == 'yes' and should_move == 'no':
-    print('Use glue')
-elif is_moving == 'no' and should_move == 'yes':
-    print('Use oil')
-else:
-    print("Don't touch")
+import random
+
+n = random.randint(1, 5)
+while True:
+    a = int(input('Enter a number: '))
+    if a == n:
+        print('Well done!')
+        break
+    else:
+        print('Try again.')
