e3d9432b44aae9ea1545d60c3e3157b2649feaaf
Dmitry Buevich <buevichd@gmail.com>
2023-10-31 17:11:32 +0300

    Lesson 9, class Bank (1)

--- lesson_09/bank.py
+++ lesson_09/bank.py
@@ -14,3 +14,23 @@ class BankAccount:
         self.money = 0
         self.account_number = get_random_digits(20)
         self.card_number = get_random_digits(16)
+
+
+class Bank:
+    def __init__(self):
+        self.__bank_accounts: list[BankAccount] = []
+
+    def open_account(self, card_holder) -> BankAccount:
+        account = BankAccount(card_holder)
+        self.__bank_accounts.append(account)
+        return account
+
+    def __get_account(self, account_number: str) -> BankAccount:
+        for account in self.__bank_accounts:
+            if account.account_number == account_number:
+                return account
+        # There should be raise ValueError()
+        return None
+
+    def get_all_bank_accounts(self) -> list[BankAccount]:
+        return self.__bank_accounts
