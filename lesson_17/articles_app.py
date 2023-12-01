27d29ec50bb45cfebaf01f88740c6cdbc788f814
Dmitry Buevich <buevichd@gmail.com>
2023-12-01 12:22:31 +0300

    Lesson 17

---
+++ lesson_17/articles_app.py
@@ -0,0 +1,69 @@
+from dataclasses import dataclass
+
+from flask import Flask, abort, redirect, session, request
+from flask_session import Session
+import sqlite3
+
+DATABASE_FILE = 'sqlite3.db'
+
+app = Flask(__name__)
+
+@dataclass
+class Article:
+    id: int
+    title: str
+    text: str
+    author: str
+
+
+def get_all_articles() -> list[Article]:
+    with sqlite3.connect(DATABASE_FILE) as connection:
+        execution_result = connection.execute(
+            'SELECT id, title, text, author FROM article')
+        return [Article(*values) for values in execution_result.fetchall()]
+
+
+def get_article(article_id: int) -> Article:
+    with sqlite3.connect(DATABASE_FILE) as connection:
+        execution_result = connection.execute('SELECT id, title, text, author '
+                                              'FROM article '
+                                              'WHERE id = ?', (article_id,))
+        rows = execution_result.fetchall()
+        if len(rows) != 1:
+            raise ValueError(f'Expected 1 object with id {article_id}, got {len(rows)}')
+        return Article(*rows[0])
+
+
+def save_article(article: Article):
+    with sqlite3.connect(DATABASE_FILE) as connection:
+        data = (article.title, article.text, article.author, article.id)
+        connection.execute('UPDATE article '
+                           'SET title = ?, text = ?, author = ? '
+                           'WHERE id = ?', data)
+
+
+@app.route('/')
+@app.route('/articles')
+def articles_view():
+    articles = get_all_articles()
+    articles_html = '\n'.join(
+        f'<li>{article.title}</li>'
+        for article in articles)
+
+    return f'''
+    <html>
+        <head>
+            <title>Articles APP</title>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+        </head>
+        <body>
+            <h1>All Articles</h1>
+            <ul>
+                {articles_html}
+            </ul>
+        </body>
+    </html>
+    '''
+
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
---
+++ lesson_17/data.sql
@@ -0,0 +1,14 @@
+CREATE TABLE article (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title VARCHAR,
+    "text" VARCHAR,
+    author VARCHAR
+);
+
+INSERT INTO article (title, text, author)
+VALUES
+    ('Article 1', 'Long text of Article 1', 'Judy Hops'),
+    ('Article 2', 'Long text of Article 2', 'John Doe'),
+    ('Article 3', 'Long text of Article 3', 'Bill Jakobs');
+
+SELECT * FROM article;
